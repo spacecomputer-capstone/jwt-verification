3b033f48b56280113dcb9eeb6ccd7ef0d6ac492f
shashank790 <shashank790@ucsb.edu>
2026-02-26 22:10:18 -0800

    add gunicorn in requirements.txt

--- jwt-flask/config.py
+++ jwt-flask/config.py
@@ -7,7 +7,7 @@ JWT_EXP_SECONDS = int(os.getenv("JWT_EXP_SECONDS", 60))
 
 BASE_DIR = os.path.dirname(__file__)
 
-BACKEND_PRIVATE_KEY = open(f"{BASE_DIR}/keys/backend_private.pem").read()
-BACKEND_PUBLIC_KEY = open(f"{BASE_DIR}/keys/backend_public.pem").read()
+BACKEND_PRIVATE_KEY = os.getenv("BACKEND_PRIVATE_KEY").replace("\\n", "\n") or open(f"{BASE_DIR}/keys/backend_private.pem").read()
+BACKEND_PUBLIC_KEY = os.getenv("BACKEND_PUBLIC_KEY").replace("\\n", "\n") or open(f"{BASE_DIR}/keys/backend_public.pem").read()
 
 SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
